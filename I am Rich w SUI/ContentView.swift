b95416e563f0b3d5d64d5416717dfb1b406c773f
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-05-01 23:55:11 +0300

    upd

--- I am Rich w SUI/ContentView.swift
+++ I am Rich w SUI/ContentView.swift
@@ -9,8 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack {
+            Color(.systemTeal).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text("I am Rich")
+                .font(.system(size: 40))
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
             .padding()
+                
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
