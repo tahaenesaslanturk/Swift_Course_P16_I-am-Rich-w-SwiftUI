051a25bba7d8f615a4fe59f5004b8ec50906d92c
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-05-01 23:41:11 +0300

    Initial Commit

---
+++ I am Rich w SUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I am Rich w SUI
+//
+//  Created by Taha Enes Aslantürk on 1.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I am Rich w SUI/I_am_Rich_w_SUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich_w_SUIApp.swift
+//  I am Rich w SUI
+//
+//  Created by Taha Enes Aslantürk on 1.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Rich_w_SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
